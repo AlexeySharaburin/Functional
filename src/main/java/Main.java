da8da4b54f9861b57e1bf2d6197c44fb984353d0
Alexey Sharaburin <alexey.sharaburin@gmail.com>
2020-12-22 16:26:39 +0100

    Correct Functional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,29 +7,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        //нет использования состояний
-        final String yourWords;
-        final String stringAborigens = "sdfaljhds ksjdfhlksjd jsdhflasdj sdvjbalsc vb,nxzcmvb sdjfuw saytqwi uqwrywieu zxnbv, vajscb vbz,n nbxv ,z asbva, uiwerteuwfg avlsv aljgl VBXZL bv,ZD JDSGlw";
+        String stringAborigens = "sdfaljhds ksjdfhlksjd jsdhflasdj sdvjbalsc vb,nxzcmvb sdjfuw saytqwi uqwrywieu zxnbv, vajscb vbz,n nbxv ,z asbva, uiwerteuwfg avlsv aljgl VBXZL bv,ZD JDSGlw";
 
         System.out.println("В качестве дополнения к тексу абригенов введите своё высказывание:");
-        yourWords = scanner.nextLine();
+        String yourWords = scanner.nextLine();
 
         // детерминированность
         Function<String, String[]> splitWords = string -> string.split("\\s");
 
-        // детерминированность
-        Function<String[], String[]> sort = new Function<String[], String[]>() {
-            @Override
-            public String[] apply(String[] strings) {
-                Arrays.sort(strings);
-                return strings;
-            }
-        };
-
         System.out.println("\nВаш новый словарь для общения с аборигенами:");
 
         // Монада
-        Arrays.stream(sort.apply(sort.apply(splitWords.apply(stringAborigens + yourWords))))
+        Arrays.stream((splitWords.apply(stringAborigens + yourWords)))
+                .sorted()
                 .map(w -> w + " - значение этого слова пока неизвестно!")
                 .forEach(System.out::println);
 
